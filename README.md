7d0ae49179510ba5c0994d760f91980370139a0f
John Gonon <j.gonon@gmail.com>
2020-03-14 21:59:43 +0100

    Updated for Leviathan 1.0.56049

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # pp-full-mutations
 
-Current Phoenix Point release is limited to 2 body parts only allowed for mutation.
+Current Phoenix Point release is limited to 2 body parts only allowed for augmentations.
 
-This "mod" allows all three body parts to receive a mutation.
+This "mod" allows all three body parts to receive an augmentation.
 
 #### Code change
 
-The code contains a constant "MAX_MUTATIONS" set to 2. Unfortunately it is impossible to only change the value of a constant because it is "inlined" in the code (not used at runtime).
+The code contains a constant "MAX_AUGMENTATIONS" set to 2. Unfortunately it is impossible to only change the value of a constant because it is "inlined" in the code (not used at runtime).
 So I had to settle to modify the method where it matters (fortunately a single one).
 
 This also means that if they ever change the code for this method, the "mod" will fail.
--- pp-full-mutations/pp-full-mutations.cs
+++ pp-full-mutations/pp-full-mutations.cs
@@ -4,7 +4,9 @@ using Harmony;
 using PhoenixPoint.Common.Core;
 using PhoenixPoint.Common.Entities.Addons;
 using PhoenixPoint.Common.Entities.GameTags;
+using PhoenixPoint.Common.Entities.Items;
 using PhoenixPoint.Geoscape.Entities;
+using PhoenixPoint.Geoscape.View.ViewControllers.AugmentationScreen;
 using PhoenixPoint.Geoscape.View.ViewModules;
 using PhoenixPointModLoader;
 using System;
@@ -20,7 +22,6 @@ namespace pantolomin.phoenixPoint.fullMutation
         public void Initialize()
         {
             HarmonyInstance harmonyInstance = HarmonyInstance.Create(typeof(Mod).Namespace);
-            // Dirty: We re-execute the method
             Patch(harmonyInstance, typeof(UIModuleMutate), "InitCharacterInfo", "Override_InitCharacterInfo");
         }
 
@@ -30,53 +31,76 @@ namespace pantolomin.phoenixPoint.fullMutation
         // ******************************************************************************************************************
         // ******************************************************************************************************************
 
-        private const int MAX_MUTATIONS = 3;
+        private const int MAX_AUGMENTATIONS = 3;
 
         public static bool Override_InitCharacterInfo(UIModuleMutate __instance,
-            GeoCharacter ____currentCharacter,
-            ref int ____currentCharacterMutations,
-            Dictionary<AddonSlotDef, UIModuleMutationSection> ____mutationSections)
+            ref int ____currentCharacterAugmentsAmount,
+			Dictionary<AddonSlotDef, UIModuleMutationSection> ____augmentSections,
+			GameTagDef ____bionicsTag,
+			GameTagDef ____mutationTag)
         {
-            int i;
-            ____currentCharacterMutations = 0;
-            GameTagDef anuMutationTag = GameUtl.GameComponent<SharedData>().SharedGameTags.AnuMutationTag;
-            foreach (GeoItem armourItem in ____currentCharacter.ArmourItems)
-            {
-                if (!armourItem.ItemDef.Tags.Contains(anuMutationTag))
-                {
-                    continue;
-                }
-                ____currentCharacterMutations++;
-            }
-            foreach (KeyValuePair<AddonSlotDef, UIModuleMutationSection> mutationSection in ____mutationSections)
-            {
-                mutationSection.Value.ResetContainer(____currentCharacterMutations < MAX_MUTATIONS);
-            }
-            foreach (GeoItem geoItem in ____currentCharacter.ArmourItems)
-            {
-                if (!geoItem.ItemDef.Tags.Contains(anuMutationTag))
-                {
-                    continue;
-                }
-                AddonDef.RequiredSlotBind[] requiredSlotBinds = geoItem.ItemDef.RequiredSlotBinds;
-                for (i = 0; i < (int)requiredSlotBinds.Length; i++)
-                {
-                    AddonDef.RequiredSlotBind requiredSlotBind = requiredSlotBinds[i];
-                    if (____mutationSections.ContainsKey(requiredSlotBind.RequiredSlot))
-                    {
-                        ____mutationSections[requiredSlotBind.RequiredSlot].SetMutationUsed(geoItem.ItemDef);
-                    }
-                }
-            }
-            string str = __instance.XoutOfY.Localize(null);
-            str = str.Replace("{0}", ____currentCharacterMutations.ToString());
-            str = str.Replace("{1}", MAX_MUTATIONS.ToString());
-            __instance.MutationsAvailableValue.text = str;
-            __instance.MutationsAvailableValue.GetComponent<UIColorController>().SetWarningActive(MAX_MUTATIONS <= ____currentCharacterMutations, false);
-
-            // Skip the method
-            return false;
-        }
+			int i;
+			bool flag;
+			bool flag1;
+			____currentCharacterAugmentsAmount = AugmentScreenUtilities.GetNumberOfAugments(__instance.CurrentCharacter);
+			bool flag2 = ____currentCharacterAugmentsAmount < MAX_AUGMENTATIONS;
+			foreach (KeyValuePair<AddonSlotDef, UIModuleMutationSection> _augmentSection in ____augmentSections)
+			{
+				AugumentSlotState augumentSlotState = AugumentSlotState.Available;
+				string localizationKey = null;
+				ItemDef augmentAtSlot = AugmentScreenUtilities.GetAugmentAtSlot(__instance.CurrentCharacter, _augmentSection.Key);
+				if (augmentAtSlot != null)
+				{
+					flag = augmentAtSlot.Tags.Contains(____bionicsTag);
+				}
+				else
+				{
+					flag = false;
+				}
+				bool flag3 = flag;
+				if (augmentAtSlot != null)
+				{
+					flag1 = augmentAtSlot.Tags.Contains(____mutationTag);
+				}
+				else
+				{
+					flag1 = false;
+				}
+				bool flag4 = flag1;
+				if (flag3)
+				{
+					localizationKey = __instance.LockedDueToBionicsKey.LocalizationKey;
+					augumentSlotState = AugumentSlotState.BlockedByPermenantAugument;
+				}
+				else if (!flag2 && !flag4)
+				{
+					localizationKey = __instance.LockedDueToLimitKey.LocalizationKey;
+					augumentSlotState = AugumentSlotState.AugumentationLimitReached;
+				}
+				_augmentSection.Value.ResetContainer(augumentSlotState, localizationKey);
+			}
+			foreach (GeoItem armourItem in __instance.CurrentCharacter.ArmourItems)
+			{
+				if (!armourItem.ItemDef.Tags.Contains(____mutationTag))
+				{
+					continue;
+				}
+				AddonDef.RequiredSlotBind[] requiredSlotBinds = armourItem.ItemDef.RequiredSlotBinds;
+				for (i = 0; i < (int)requiredSlotBinds.Length; i++)
+				{
+					AddonDef.RequiredSlotBind requiredSlotBind = requiredSlotBinds[i];
+					if (____augmentSections.ContainsKey(requiredSlotBind.RequiredSlot))
+					{
+						____augmentSections[requiredSlotBind.RequiredSlot].SetMutationUsed(armourItem.ItemDef);
+					}
+				}
+			}
+			__instance.MutationsAvailableValue.text = __instance.XoutOfY.Localize(null)
+				.Replace("{0}", ____currentCharacterAugmentsAmount.ToString())
+				.Replace("{1}", MAX_AUGMENTATIONS.ToString()); ;
+			__instance.MutationsAvailableValue.GetComponent<UIColorController>().SetWarningActive(MAX_AUGMENTATIONS <= ____currentCharacterAugmentsAmount, false);
+			return false;
+		}
 
         // ******************************************************************************************************************
         // ******************************************************************************************************************
